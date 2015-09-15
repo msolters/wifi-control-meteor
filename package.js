ff3e13b0f9af7377a519b7971f9d3bdf7e319b0c
Mark Solters <msolters@driblet.io>
2015-09-15 18:19:02 -0400

    Added GIT url to package.

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'msolters:wifi-control',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Connect or disconnect the host to wireless networks.  Useful for partially or fully offline apps, such as Electrified apps.',
+  summary: 'Scan for, connect to, or disconnect from WiFi networks.  Useful for partially or fully offline apps, such as Electrified apps.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/msolters/wifi-control',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
