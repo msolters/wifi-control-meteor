668dca1abe871bf2bd5c27ba8b51272499adbb7a
Mark Solters <msolters@driblet.io>
2015-09-17 17:52:32 -0400

    v0.1.1, moved NPM dependency to published version # and updated GIT url.

--- package.js
+++ package.js
@@ -1,24 +1,23 @@
 Package.describe({
   name: 'msolters:wifi-control',
-  version: '0.0.1',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Scan for, connect to, or disconnect from WiFi networks.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/msolters/wifi-control',
+  git: 'https://github.com/msolters/wifi-control-meteor',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Npm.depends({
-  "node-wifiscanner2": "1.1.0",
-  "exec-sync": "0.1.6"
+  "wifi-control": "0.1.1"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
   api.use('coffeescript');
-  api.addFiles( ['lib/wifi-control.coffee', 'lib/wifi-control-server.coffee'], 'server');
+  api.addFiles( ['wifi-control.coffee'], 'server');
   api.export('WiFiControl', 'server');
 });
 
